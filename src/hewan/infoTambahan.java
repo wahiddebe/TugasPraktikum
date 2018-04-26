e4fe8a033f5fcb1ad4574416c0a81bc78127b41d
irgantaraa <irgantaraa@gmail.com>
2018-04-26 11:40:03 +0700

    tugas modul 6

---
+++ src/hewan/infoTambahan.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hewan;
+
+/**
+ *
+ * @author M.'Azzam Hanif
+ */
+public class infoTambahan {
+    private String NamaKucing, sound;
+    private int umur;
+    
+        public void Kucing(String name){
+		NamaKucing = name;
+	}
+
+	public void Suara(String suara){
+		sound = suara;
+	}
+
+	public void Umur(int ageCat){
+		umur = ageCat;
+	}
+        
+        public void cingku(){
+		System.out.println("Kucing saya namanya "+NamaKucing);
+	}
+
+	public void arasu(){
+		System.out.println("Dia memiliki suara "+sound);
+	}
+
+	public void rumu(){
+		System.out.println(NamaKucing+" berumur "+umur+" Bulan");
+	}
+}
