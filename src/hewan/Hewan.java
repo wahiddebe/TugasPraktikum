60b5205194779991855152322b768057c1aea3f1
irgantaraa <irgantaraa@gmail.com>
2018-04-26 11:33:37 +0700

    tugas modul 6

---
+++ src/hewan/Hewan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hewan;
+
+/**
+ *
+ * @author M.'Azzam Hanif
+ */
+public class Hewan extends infoTambahan {// Biar bisa mengabil data dari class infoTambahan
+
+     
+   
+    
+    public static void main(String[] args) {
+
+            infoTambahan mykucing = new infoTambahan();
+            mykucing.Kucing("Hamtaro");
+            mykucing.Suara("Mbee");
+            mykucing.Umur(10);
+            
+            mykucing.cingku();
+            mykucing.arasu();
+            mykucing.rumu();
+	}
+    
+}
